813830ed1b271fa93a90d1b73d919d9d7d6c4545
Bao Trong <nhbaotrong@gmail.com>
2024-11-10 03:03:14 +0700

    [add] complete algorithm calculator day

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { MantineProvider } from '@mantine/core'
 import './App.css'
 import '@mantine/core/styles.css'
+import dayjs from 'dayjs'
+import { useEffect, useRef } from 'react'
+
+// const days = Math.floor(durationInSeconds / (24 * 60 * 60));
+// const hours = Math.floor((durationInSeconds % (24 * 60 * 60)) / (60 * 60));
+// const minutes = Math.floor((durationInSeconds % (60 * 60)) / 60);
+// const seconds = durationInSeconds % 60;
 
 function App() {
+  const intervalRef = useRef<ReturnType<typeof setInterval>>()
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      const now = dayjs();
+      const durationInSeconds = now.diff(dayjs("2023-09-13"), 'second');
+      console.log(durationInSeconds);
+    }, 1000)
+
+    return () => clearInterval(intervalRef.current)
+  }, [])
 
   return (
     <MantineProvider defaultColorScheme='light'>
